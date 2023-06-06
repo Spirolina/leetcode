a18c5076dd0dce8d138f113d0ef941976c7552f6
Arda Işık <aromax30@gmail.com>
2023-06-06 12:48:47 +0300

    Time: 121 ms (7.13%), Space: 49.4 MB (9.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -15,8 +15,17 @@ var topKFrequent = function (nums, k) {
             if (outs.length === k) {
                 if (count > minCount) {
                     outs.splice(0, 1, [nums[i], count]);
-                }
+                    
+                    outs = outs.sort((a, b) => a[1] - b[1])
+                    minCount = outs[0][1]
+                    
+                } 
             }
+            else {
+                outs.push([nums[i], count])
+            }
+           
+            break;
         }
         if (nums[i] === nums[i + 1]) {
             count++;
@@ -24,22 +33,28 @@ var topKFrequent = function (nums, k) {
 
         if (nums[i] !== nums[i + 1]) {
             if (outs.length === k) {
+                minCount = outs[0][1]
+
                 if (count <= minCount) {
                     count = 1;
                     continue;
                 }
+
                 outs.splice(0, 1, [nums[i], count]);
+                
+                outs = outs.sort((a, b) => a[1] - b[1])
                 count = 1;
                 continue;
             }
+
             outs.push([nums[i], count]);
+            outs = outs.sort((a, b) => a[1] - b[1])
+
             count = 1;
         }
-        if (outs.length !== 0) {
-            outs = outs.sort((a, b) => a[1] - b[1])
-            minCount = outs[0][1]
-        }
+
     }
+    
     result = outs.map(a => a[0])
     return result;
     
