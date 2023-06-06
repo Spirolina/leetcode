371afc927d13ff62735b34a79336f1002c32d0b1
Arda Işık <aromax30@gmail.com>
2023-06-06 21:01:14 +0300

    Time: 125 ms (5.94%), Space: 52.2 MB (34.97%) - LeetHub

---
+++ 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
@@ -0,0 +1,17 @@
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+    let result = false;
+    while (obj !== null && obj !== undefined) {
+        if (obj.constructor === classFunction) {
+            result = true;
+            break;
+        }
+        obj = obj.__proto__;
+
+    }
+    return result;
+};
