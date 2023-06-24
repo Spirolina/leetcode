d5ac5843ba6784882a1ab5aa6b6b3ff8d0829247
Arda Işık <g.isik.arda@gmail.com>
2023-06-24 21:57:17 +0300

    Time: 214 ms (5.08%), Space: 51.4 MB (5.44%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -1,41 +1,36 @@
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function(l1, l2) {
-  let dummy = new ListNode();
-  let curr = dummy;
-  let carry = 0;
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+var addTwoNumbers = function (l1, l2) {
+    let result = new ListNode();
+    let seek = result;
+    let addition = 0;
+    while (l1 || l2) {
+        console.log(l1.val, l2.val)
+        let sum = l1.val + l2.val + addition;
+        addition = Math.floor(sum / 10);
+        seek.val = sum % 10;
+        if(!l1.next && !l2.next){
+            if(addition){
+                seek.val = sum % 10;
+                seek.next = new ListNode(addition);
+            } 
+             break;
 
-  while (l1 !== null || l2 !== null) {
-    let sum = carry;
+        }
 
-    if (l1 !== null) {
-      sum += l1.val;
-      l1 = l1.next;
-    }
-
-    if (l2 !== null) {
-      sum += l2.val;
-      l2 = l2.next;
-    }
-
-    curr.next = new ListNode(sum % 10);
-    curr = curr.next;
-    carry = Math.floor(sum / 10);
-  }
-
-  if (carry > 0) {
-    curr.next = new ListNode(carry);
-  }
-
-  return dummy.next;
+        seek.next = new ListNode();
+        seek = seek.next;
+        l1 = l1.next ? l1.next : new ListNode();
+        l2 = l2.next ? l2.next : new ListNode();
+   }
+   return result
 };
