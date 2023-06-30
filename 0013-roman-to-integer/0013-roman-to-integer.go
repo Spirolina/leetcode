3cc102968048a55b4f49414da61cf81f57f96092
Arda Işık <g.isik.arda@gmail.com>
2023-06-30 13:43:55 +0300

    Time: 14 ms (23.68%), Space: 3 MB (14.67%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -1,60 +1,55 @@
-type Roman struct {
-	c byte
-	v int
-}
-
 func romanToInt(s string) int {
 	sum := 0
-	var prev Roman
+	var prev int
 	for i := 0; i < len(s); i++ {
 		fmt.Println(sum)
 		switch s[i] {
 		case 'I':
 			sum += 1
-			prev = Roman{s[i], 1}
+			prev = 1
 		case 'V':
 			total := 5
-			if prev.v < 5 {
-				total -= 2 * prev.v
+			if prev < 5 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 5}
+			prev = 5
 
 		case 'X':
 			total := 10
-			if prev.v < 10 {
-				total -= 2 * prev.v
+			if prev < 10 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 10}
+			prev = 10
 		case 'L':
 			total := 50
-			if prev.v < 50 {
-				total -= 2 * prev.v
+			if prev < 50 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 50}
+			prev = 50
 		case 'C':
 			total := 100
-			if prev.v < 100 {
-				total -= 2 * prev.v
+			if prev < 100 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 100}
+			prev = 100
 		case 'D':
 			total := 500
-			if prev.v < 500 {
-				total -= 2 * prev.v
+			if prev < 500 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 500}
+			prev = 500
 		case 'M':
 			total := 1000
-			if prev.v < 1000 {
-				total -= 2 * prev.v
+			if prev < 1000 {
+				total -= 2 * prev
 			}
 			sum += total
-			prev = Roman{s[i], 1000}
+			prev = 1000
 		default:
 			fmt.Println("unknown")
 		}
